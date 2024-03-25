a610cf2ef50d50f3c2e9b2a1be39bf3520b1632a
netoetec <tagudelaconta123@gmail.com>
2024-03-25 20:30:42 -0300

    Alterado as imagens e o JSON

--- Games/Models/Jogo.cs
+++ Games/Models/Jogo.cs
@@ -4,6 +4,8 @@ public class Jogo
 {
     public int Numero { get; set; } 
     public string Nome { get; set; }
+    public string Desenvolvedor { get; set; }
+    public string DataLancamento { get; set; }
     public string Descricao { get; set; }
     public List<string> Plataforma { get; set; }
     public string Imagem { get; set; }
